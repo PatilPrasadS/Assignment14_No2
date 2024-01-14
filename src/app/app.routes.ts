47e768a9e7298694f1fed5b2d07fa94e3da24a7b
PatilPrasadS <prsyy1234@gmail.com>
2024-01-14 22:45:31 +0530

    Routes with nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { BooksComponent } from './books/books.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'technologies',
+        component : TechnologiesComponent
+    },
+    {
+        path : 'books',
+        component : BooksComponent
+    }
+];
